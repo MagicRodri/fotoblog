0cbcfe54ea03ea0412523ca29674c14699d505e7
Rodrigue Roland CODO <codorol22@gmail.com>
2022-07-30 20:18:22 +0500

    lines deletion

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,6 @@ from django.urls import reverse
 from pathlib import Path
 from django.core.files import File
 from django.conf import settings
-from django.db.models.signals import pre_save
 
 # Create your models here.
 class User(AbstractUser):
@@ -28,12 +27,4 @@ class User(AbstractUser):
 
     def get_absolute_url(self):
         return reverse("profile-view")
-
-
-
-
-def set_default_picture(sender,instance,*args,**kargs):
-    if instance.picture is None:
-        instance.picture = instance.DEFAULT_PICTURE
-
-pre_save.connect(set_default_picture,sender=User)           
+       

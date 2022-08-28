7791d691145bf10174414e8c6f1ae8f1e0393452
Rodrigue Roland CODO <codorol22@gmail.com>
2022-08-28 12:28:43 +0500

    Converted Post.content into Quill type

--- fotoblog/settings.py
+++ fotoblog/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #third-party
+    'django_quill',
+
+
     'accounts',
     'blog'
 ]

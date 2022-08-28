a5ec56a1eb4a3556ecd124079a4ba62bf1b44a07
Rodrigue Roland CODO <codorol22@gmail.com>
2022-08-29 01:40:56 +0500

    Preparing for production
    Sensitive data hidden from settings.py

--- fotoblog/settings.py
+++ fotoblog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%f_c-en@w(4jcj!&xv41apeyvsmk))q7i7_uozy2qq+v-@lokc'
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-%f_c-en@w(4jcj!&xv41apeyvsmk))q7i7_uozy2qq+v-@lokc')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG')) == '1'
 
 ALLOWED_HOSTS = []
+if not DEBUG:
+    ALLOWED_HOSTS += [os.environ.get('ALLOWED_HOSTS')]
 
 
 # Application definition

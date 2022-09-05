2bcb9322d970fdd1acf184b1a016f942ba1e5b11
Rodrigue Roland CODO <codorol22@gmail.com>
2022-09-05 21:49:02 +0500

    Added logo to the header and changed layout on mobile

--- static_dir/base/script.js
+++ static_dir/base/script.js
@@ -8,7 +8,7 @@ hamburger.addEventListener('click',() =>{
     navMenu.classList.toggle('active')
 });
 
-document.querySelectorAll('nav a').forEach(n => n.addEventListener('click', () => {
+document.querySelectorAll('header a').forEach(n => n.addEventListener('click', () => {
     hamburger.classList.remove('active');
     navMenu.classList.remove('active')
 }));

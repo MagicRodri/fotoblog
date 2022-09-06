67926f4fea08eead0ccc1042f8adf01ab3c7efb8
Rodrigue Roland CODO <codorol22@gmail.com>
2022-09-06 21:40:23 +0500

    Added content to the cover page and made some layout

--- fotoblog/views.py
+++ fotoblog/views.py
@@ -5,20 +5,6 @@ from django.db.models import Q
 from blog.models import Photo,Post
 from itertools import chain
 
-@login_required
 def home_view(request):
-    user = request.user
 
-    posts_lookup = Q(author__in = user.follows.all()) | Q(author = user)
-    posts = Post.objects.filter(posts_lookup)
-    
-    photos_lookups = Q(uploader__in = user.follows.all()) | Q(uploader = user)
-    photos = Photo.objects.filter(photos_lookups).exclude(post__in = posts)
-    
-    posts_and_photos = sorted(chain(posts,photos),key=lambda instance : instance.timestamp,reverse = True)
-    context = {
-        'posts_and_photos' : posts_and_photos,
-        'photos': photos,
-        'posts' : posts
-    }
-    return render(request,'home.html',context = context)
+    return render(request,'home.html')

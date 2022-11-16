0a453c73ad754d91f4a3ce8658c93649378a7944
Rodrigue Roland CODO <codorol22@gmail.com>
2022-11-16 23:46:19 +0500

    User model updated

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,7 @@
-from pyexpat import model
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from pyexpat import model
 
 User = get_user_model()
 
@@ -33,7 +33,7 @@ class PpUploadForm(forms.ModelForm):
 class EditProfileForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['username','first_name','last_name','email','role']
+        fields = ['username','first_name','last_name','email','role','description','location','website']
 
 class FollowCreatorForm(forms.ModelForm):
 

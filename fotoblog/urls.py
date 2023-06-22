ca6332472b3166997be35afb0807b76fa1bbd8e5
Rodrigue CODO <codorol22@gmail.com>
2023-06-22 23:23:20 +0500

    Fix import

--- fotoblog/urls.py
+++ fotoblog/urls.py
@@ -21,13 +21,12 @@ from django.urls import include, path
 import accounts.urls
 import blog.urls
 
-from .views import home_view
-
 urlpatterns = [
-    path('',include(blog.urls)),
-    path('accounts/',include(accounts.urls)),
+    path('', include(blog.urls)),
+    path('accounts/', include(accounts.urls)),
     path('admin/', admin.site.urls),
 ]
 
-if settings.DEBUG :
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
